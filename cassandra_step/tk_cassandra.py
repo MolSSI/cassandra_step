e7f15badd38475973abba987bbd03d471d344537
Eliseo Marin-Rimoldi <emarinri@nd.edu>
2019-04-03 17:24:00 -0400

    Create basic notebook

--- cassandra_step/tk_cassandra.py
+++ cassandra_step/tk_cassandra.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """The graphical part of a Cassandra step"""
 
@@ -29,6 +30,7 @@ class TkCassandra(molssi_workflow.TkNode):
 
     def create_dialog(self):
         """Create the dialog!"""
+
         self.dialog = Pmw.Dialog(
             self.toplevel,
             buttons=('OK', 'Help', 'Cancel'),
@@ -36,45 +38,97 @@ class TkCassandra(molssi_workflow.TkNode):
             master=self.toplevel,
             title='Edit Cassandra step',
             command=self.handle_dialog)
+
         self.dialog.withdraw()
 
-        # The information about widgets is held in self['xxxx'], i.e. this
-        # class is in part a dictionary of widgets. This makes accessing
-        # the widgets easier and allows loops, etc.
+        nb = Pmw.NoteBook(self.dialog.interior())
+        p1 = nb.add('Page 1')
 
-        # Create a frame to hold everything. This is always called 'frame'.
-        self['frame'] = ttk.Frame(self.dialog.interior())
-        self['frame'].pack(expand=tk.YES, fill=tk.BOTH)
-        # Shortcut for parameters
-        P = self.node.parameters
+        nb.pack(padx=5, pady=5, fill=tk.BOTH, expand=1)
 
-        # # Frame to isolate widgets
-        # self['thermo_state'] = ttk.LabelFrame(
-        #     self['frame'], borderwidth=4, relief='sunken',
-        #     text='Thermodynamic state', labelanchor='n', padding=10
-        # )
+        # tk.Button(p1, text='This is text on page 1', fg='blue').pack(pady=40)
+        # c = tk.Canvas(p2, bg='gray30')
+        # w = c.winfo_reqwidth()
+        # h = c.winfo_reqheight()
+        # c.create_oval(10, 10, w - 10, h - 10, fill='DeepSkyBlue1')
+        # c.create_text(w / 2, h / 2, text='This is text on a canvas', fill='white',
+        #               font=('Verdana', 14, 'bold'))
+        # c.pack(fill=tk.BOTH, expand=1)
 
-        # self['temperature'] = P['temperature'].widget(self['thermo_state'])
-        # self['num_mols'] = P['num_mols'].widget(self['thermo_state'])
+        # self.dialog = Pmw.Dialog(
+        #     self.toplevel,
+        #     buttons=('OK', 'Help', 'Cancel'),
+        #     defaultbutton='OK',
+        #     master=self.toplevel,
+        #     title='Edit Cassandra step',
+        #     command=self.handle_dialog)
+        # self.dialog.withdraw()
 
-        # row = 0
-        # self['temperature'].grid(row=row, column=0, sticky=tk.W)
-        # row += 1
-        # self['num_mols'].grid(row=row, column=0, sticky=tk.W)
-        # row += 1
+        #         # The information about widgets is held in self['xxxx'], i.e. this
+        #         # class is in part a dictionary of widgets. This makes accessing
+        #         # the widgets easier and allows loops, etc.
 
-        # mw.align_labels(
-        #     (self['temperature'],
-        #      self['num_mols'],
-        #      )
-        # )
+        #         # Create a frame to hold everything. This is always called 'frame'.
+        #         self['frame'] = ttk.Frame(self.dialog.interior())
+        #         self['frame'].pack(expand=tk.YES, fill=tk.BOTH)
+        #         # Shortcut for parameters
+        #         P = self.node.parameters
 
-        # The create the widgets
-        for key in P:
-            self[key] = P[key].widget(self['frame'])
+        # # *********************************
 
-        # and lay them out
-        self.reset_dialog()
+        #     # Frame to isolate widgets
+        #         self['thermo_state'] = ttk.LabelFrame(
+        #             self['frame'], borderwidth=4, relief='sunken',
+        #             text='Thermodynamic state', labelanchor='n', padding=10
+        #         )
+
+        #         self['thermo_state'].pack(expand=tk.YES, fill=tk.BOTH)
+
+        # # *********************************
+
+        #         # Frame to isolate widgets
+        #         self['general_info'] = ttk.LabelFrame(
+        #             self['frame'], borderwidth=4, relief='sunken',
+        #             text='General simulation information', labelanchor='n', padding=10
+        #         )
+        #         self['general_info'].pack(expand=tk.YES, fill=tk.BOTH)
+
+        # # *********************************
+
+        #         self['temperature'] = P['temperature'].widget(self['thermo_state'])
+        #         self['num_mols'] = P['num_mols'].widget(self['thermo_state'])
+
+        #         row = 0
+        #         self['temperature'].grid(row=row, column=0, sticky=tk.W)
+        #         row += 1
+        #         self['num_mols'].grid(row=row, column=0, sticky=tk.W)
+
+        # # *********************************
+
+        #         self['run_name'] = P['run_name'].widget(self['general_info'])
+        #         self['run_type'] = P['run_type'].widget(self['general_info'])
+
+        #         row = 0
+        #         self['run_name'].grid(row=row, column=0, sticky=tk.W)
+        #         row += 1
+        #         self['run_type'].grid(row=row, column=0, sticky=tk.W)
+
+        # # *********************************
+
+        #         mw.align_labels(
+        #             (self['temperature'],
+        #              self['num_mols'],
+        #              self['run_name'],
+        #              self['run_type']
+        #              )
+        #         )
+
+        #         # The create the widgets
+        #         # for key in P:
+        #         #     self[key] = P[key].widget(self['frame'])
+
+        #         # and lay them out
+        #         self.reset_dialog()
 
     def reset_dialog(self, widget=None):
         """Layout the widgets in the dialog
@@ -85,24 +139,25 @@ class TkCassandra(molssi_workflow.TkNode):
         """
 
         # Remove any widgets previously packed
-        frame = self['frame']
-        for slave in frame.grid_slaves():
-            slave.grid_forget()
+        # frame = self['frame']
+        # for slave in frame.grid_slaves():
+        #     slave.grid_forget()
 
-        # Shortcut for parameters
-        P = self.node.parameters
+        # # Shortcut for parameters
+        # P = self.node.parameters
 
-        # keep track of the row in a variable, so that the layout is flexible
-        # if e.g. rows are skipped to control such as 'method' here
-        row = 0
-        widgets = []
-        for key in P:
-            self[key].grid(row=row, column=0, sticky=tk.EW)
-            widgets.append(self[key])
-            row += 1
+        # # keep track of the row in a variable, so that the layout is flexible
+        # # if e.g. rows are skipped to control such as 'method' here
+        # row = 0
+        # widgets = []
+        # for key in P:
+        #     self[key].grid(row=row, column=0, sticky=tk.EW)
+        #     widgets.append(self[key])
+        #     row += 1
 
-        # Align the labels
-        mw.align_labels(widgets)
+        # # Align the labels
+        # mw.align_labels(widgets)
+        pass
 
     def right_click(self, event):
         """Probably need to add our dialog...
