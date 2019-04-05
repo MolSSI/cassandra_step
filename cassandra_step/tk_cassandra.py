2795c7a357babfc1a63b73a38f08b32bcbe033e1
Eliseo Marin <meliseo@vt.edu>
2019-04-04 23:47:41 -0400

    Notebook works

--- cassandra_step/tk_cassandra.py
+++ cassandra_step/tk_cassandra.py
@@ -31,104 +31,74 @@ class TkCassandra(molssi_workflow.TkNode):
     def create_dialog(self):
         """Create the dialog!"""
 
+        sections = {'general_info': 'General information', 'thermo_state': 'Thermodynamic state', 'energy_calc': 'Energy calculation', 'probability': 'Probabilities', 'initial_config': 'Initial configuration', 'output_prop': 'Output properties', 'other': 'Other'}
+
+        """Create the dialog!"""
         self.dialog = Pmw.Dialog(
             self.toplevel,
             buttons=('OK', 'Help', 'Cancel'),
-            defaultbutton='OK',
             master=self.toplevel,
-            title='Edit Cassandra step',
+            title='Edit Energy step',
             command=self.handle_dialog)
-
         self.dialog.withdraw()
+        P = self.node.parameters
 
-        nb = Pmw.NoteBook(self.dialog.interior())
-        p1 = nb.add('Page 1')
+        # The tabbed notebook
+        notebook = ttk.Notebook(self.dialog.interior())
+        notebook.pack(side='top', fill=tk.BOTH, expand=1)
+        self._widget['notebook'] = notebook
 
-        nb.pack(padx=5, pady=5, fill=tk.BOTH, expand=1)
+        # Main frame holding the widgets
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='General information', sticky=tk.NW)
 
-        # tk.Button(p1, text='This is text on page 1', fg='blue').pack(pady=40)
-        # c = tk.Canvas(p2, bg='gray30')
-        # w = c.winfo_reqwidth()
-        # h = c.winfo_reqheight()
-        # c.create_oval(10, 10, w - 10, h - 10, fill='DeepSkyBlue1')
-        # c.create_text(w / 2, h / 2, text='This is text on a canvas', fill='white',
-        #               font=('Verdana', 14, 'bold'))
-        # c.pack(fill=tk.BOTH, expand=1)
+        run_type_label = ttk.Label(frame, text='Run type: ')
+        run_type = ttk.Combobox(
+            frame,
+            state='readonly',
+            values=P["run_type"]["enumeration"])
+        run_type_label.grid(row=0, column=0, columnspan=2, sticky=tk.E)
+        run_type.grid(row=0, column=2, sticky=tk.W)
 
-        # self.dialog = Pmw.Dialog(
-        #     self.toplevel,
-        #     buttons=('OK', 'Help', 'Cancel'),
-        #     defaultbutton='OK',
-        #     master=self.toplevel,
-        #     title='Edit Cassandra step',
-        #     command=self.handle_dialog)
-        # self.dialog.withdraw()
+        sim_length_label = ttk.Label(frame, text='Simulation length: ')
+        sim_length = ttk.Entry(frame, width=15)
+        sim_length_units = ttk.Combobox(
+            frame,
+            state='readonly',
+            values=P["sim_length_units"]["enumeration"])
+        sim_length_label.grid(row=2, column=0, columnspan=2, sticky=tk.E)
+        sim_length.grid(row=2, column=2, sticky=tk.W)
+        sim_length_units.grid(row=2, column=3, columnspan=2, sticky=tk.E)
 
-        #         # The information about widgets is held in self['xxxx'], i.e. this
-        #         # class is in part a dictionary of widgets. This makes accessing
-        #         # the widgets easier and allows loops, etc.
+        seeds_label = ttk.Label(frame, text='Random seeds: ')
+        seeds_label_one = ttk.Entry(frame, width=15)
+        seeds_label_two = ttk.Entry(frame, width=15)
+        seeds_label.grid(row=3, column=0, columnspan=2, sticky=tk.E)
+        seeds_label_one.grid(row=3, column=2, sticky=tk.W)
+        seeds_label_two.grid(row=3, column=3, columnspan=2, sticky=tk.E)
 
-        #         # Create a frame to hold everything. This is always called 'frame'.
-        #         self['frame'] = ttk.Frame(self.dialog.interior())
-        #         self['frame'].pack(expand=tk.YES, fill=tk.BOTH)
-        #         # Shortcut for parameters
-        #         P = self.node.parameters
+        freq_xyz_label = ttk.Label(frame, text='Coordinate output frequency: ')
+        freq_xyz = ttk.Entry(frame, width=15)
+        freq_xyz_label.grid(row=4, column=0, columnspan=2, sticky=tk.E)
+        freq_xyz.grid(row=4, column=2, sticky=tk.W)
 
-        # # *********************************
+        freq_thermo_label = ttk.Label(frame, text='Property output frequency: ')
+        freq_thermo = ttk.Entry(frame, width=15)
+        freq_thermo_label.grid(row=5, column=0, columnspan=2, sticky=tk.E)
+        freq_thermo.grid(row=5, column=2, sticky=tk.W)
 
-        #     # Frame to isolate widgets
-        #         self['thermo_state'] = ttk.LabelFrame(
-        #             self['frame'], borderwidth=4, relief='sunken',
-        #             text='Thermodynamic state', labelanchor='n', padding=10
-        #         )
-
-        #         self['thermo_state'].pack(expand=tk.YES, fill=tk.BOTH)
-
-        # # *********************************
-
-        #         # Frame to isolate widgets
-        #         self['general_info'] = ttk.LabelFrame(
-        #             self['frame'], borderwidth=4, relief='sunken',
-        #             text='General simulation information', labelanchor='n', padding=10
-        #         )
-        #         self['general_info'].pack(expand=tk.YES, fill=tk.BOTH)
-
-        # # *********************************
-
-        #         self['temperature'] = P['temperature'].widget(self['thermo_state'])
-        #         self['num_mols'] = P['num_mols'].widget(self['thermo_state'])
-
-        #         row = 0
-        #         self['temperature'].grid(row=row, column=0, sticky=tk.W)
-        #         row += 1
-        #         self['num_mols'].grid(row=row, column=0, sticky=tk.W)
-
-        # # *********************************
-
-        #         self['run_name'] = P['run_name'].widget(self['general_info'])
-        #         self['run_type'] = P['run_type'].widget(self['general_info'])
-
-        #         row = 0
-        #         self['run_name'].grid(row=row, column=0, sticky=tk.W)
-        #         row += 1
-        #         self['run_type'].grid(row=row, column=0, sticky=tk.W)
-
-        # # *********************************
-
-        #         mw.align_labels(
-        #             (self['temperature'],
-        #              self['num_mols'],
-        #              self['run_name'],
-        #              self['run_type']
-        #              )
-        #         )
-
-        #         # The create the widgets
-        #         # for key in P:
-        #         #     self[key] = P[key].widget(self['frame'])
-
-        #         # and lay them out
-        #         self.reset_dialog()
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='Thermodynamic state', sticky=tk.NW)
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='Initial configuration', sticky=tk.NW)
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='Energy calculation', sticky=tk.NW)
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='Probabilities', sticky=tk.NW)
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='Outputs', sticky=tk.NW)
+        frame = ttk.Frame(notebook)
+        notebook.add(frame, text='Other', sticky=tk.NW)
 
     def reset_dialog(self, widget=None):
         """Layout the widgets in the dialog
