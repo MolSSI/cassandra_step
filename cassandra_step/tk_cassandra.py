2cbc3fe42df02398c3a941eb642eebc4070452d5
Paul Saxe <psaxe@thesaxes.net>
2019-11-05 15:06:18 -0500

    Fixed dialog handling for issues with cancelling. In the process moved more functionality to tk_node in seamm.

--- cassandra_step/tk_cassandra.py
+++ cassandra_step/tk_cassandra.py
@@ -5,7 +5,6 @@
 import seamm
 from seamm_util import ureg, Q_, units_class  # noqa: F401
 import seamm_widgets as sw  # noqa: F401
-import Pmw
 import pprint  # noqa: F401
 import tkinter as tk
 import tkinter.ttk as ttk
@@ -44,19 +43,10 @@ class TkCassandra(seamm.TkNode):
 
     def create_dialog(self):
         """Create the dialog!"""
-        """Create the dialog!"""
-        self.dialog = Pmw.Dialog(
-            self.toplevel,
-            buttons=('OK', 'Help', 'Cancel'),
-            master=self.toplevel,
-            title='Edit Energy step',
-            command=self.handle_dialog
-        )
-        self.dialog.withdraw()
-        P = self.node.parameters
+        frame = super().create_dialog('Edit Cassandra Step')
 
         # The tabbed notebook
-        notebook = ttk.Notebook(self.dialog.interior())
+        notebook = ttk.Notebook(frame)
         notebook.pack(side='top', fill=tk.BOTH, expand=1)
         self._widget['notebook'] = notebook
 
@@ -83,6 +73,7 @@ class TkCassandra(seamm.TkNode):
         notebook.add(self["other"], text='Other', sticky=tk.NW)
 
         row = 0
+        P = self.node.parameters
         for k, v in P.items():
             # print(v['group'], k)
             self[k] = P[k].widget(self[v["group"]])
@@ -95,35 +86,6 @@ class TkCassandra(seamm.TkNode):
 
         # bindings...
 
-    def reset_dialog(self, widget=None):
-        """Layout the widgets in the dialog
-
-        This initial function simply lays them out row by rows with
-        aligned labels. You may wish a more complicated layout that
-        is controlled by values of some of the control parameters.
-        """
-
-        # Remove any widgets previously packed
-        # frame = self['frame']
-        # for slave in frame.grid_slaves():
-        #     slave.grid_forget()
-
-        # # Shortcut for parameters
-        # P = self.node.parameters
-
-        # # keep track of the row in a variable, so that the layout is flexible
-        # # if e.g. rows are skipped to control such as 'method' here
-        # row = 0
-        # widgets = []
-        # for key in P:
-        #     self[key].grid(row=row, column=0, sticky=tk.EW)
-        #     widgets.append(self[key])
-        #     row += 1
-
-        # # Align the labels
-        # sw.align_labels(widgets)
-        pass
-
     def right_click(self, event):
         """Probably need to add our dialog...
         """
@@ -132,46 +94,3 @@ class TkCassandra(seamm.TkNode):
         self.popup_menu.add_command(label="Edit..", command=self.edit)
 
         self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
-
-    def edit(self):
-        """Present a dialog for editing the Cassandra input
-        """
-        if self.dialog is None:
-            self.create_dialog()
-
-        self.dialog.activate(geometry='centerscreenfirst')
-
-    def handle_dialog(self, result):
-        """Handle the closing of the edit dialog
-
-        What to do depends on the button used to close the dialog. If
-        the user closes it by clicking the 'x' of the dialog window,
-        None is returned, which we take as equivalent to cancel.
-        """
-        if result is None or result == 'Cancel':
-            self.dialog.deactivate(result)
-            return
-
-        if result == 'Help':
-            # display help!!!
-            return
-
-        if result != "OK":
-            self.dialog.deactivate(result)
-            raise RuntimeError(
-                "Don't recognize dialog result '{}'".format(result)
-            )
-
-        self.dialog.deactivate(result)
-        # Shortcut for parameters
-        P = self.node.parameters
-
-        # Get the values for all the widgets. This may be overkill, but
-        # it is easy! You can sort out what it all means later, or
-        # be a bit more selective.
-        for key in P:
-            P[key].set_from_widget()
-
-    def handle_help(self):
-        """Not implemented yet ... you'll need to fill this out!"""
-        print('Help not implemented yet for Cassandra!')
